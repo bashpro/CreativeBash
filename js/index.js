38af165de2b81bc3aa771ead9ffc594d061182be
Bash <bashash10@gmail.com>
2019-01-12 19:35:09 +0000

    changes to the website

--- js/index.js
+++ js/index.js
@@ -1,21 +1,32 @@
-$(".left").hover(function(){ 
-  $(".left").addClass("active");
-  $(".middle").addClass("add");
-  $(".right").addClass("hide-menu")},
- function(){
-  $(".left").removeClass("active");
-  $(".middle").removeClass("add");
-  $(".right").removeClass("hide-menu")
-});
+$(document).ready(function(){  
+  $(".left").hover(function(){ 
+    $(".left").addClass("active");
+    $(".middle").addClass("add");
+    $(".right").addClass("hide-menu")},
+  function(){
+    $(".left").removeClass("active");
+    $(".middle").removeClass("add");
+    $(".right").removeClass("hide-menu")
+  });
 
-$(".right").hover(function(){ 
-  $(".right").addClass("active");
-  $(".middle").addClass("add");
-  $(".left").addClass("hide-menu")
-  $(".menu").addClass("hide-menu")},
- function(){
-  $(".right").removeClass("active");
-  $(".middle").removeClass("add");
-  $(".left").removeClass("hide-menu")
-  $(".menu").removeClass("hide-menu")
+  $(".right").hover(function(){ 
+    $(".right").addClass("active");
+    $(".middle").addClass("add");
+    $(".left").addClass("hide-menu")
+    $(".menu").addClass("hide-menu")},
+  function(){
+    $(".right").removeClass("active");
+    $(".middle").removeClass("add");
+    $(".left").removeClass("hide-menu")
+    $(".menu").removeClass("hide-menu")
+  });
+
+  
+  $(".p-button").click(function(){
+    $(".preview").addClass("hide");
+    if ($('.preview').hasClass("hide")){
+      $('.content-holder').removeClass("hide");
+    }
+  });
+
 });
